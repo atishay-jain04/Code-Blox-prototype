fb6681d5b7193f214f291e28e550a58dc101ef82
Atishay Jain <atishayjain0504@gmail.com>
2024-10-07 12:43:06 +0530

    first commit

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['./homepage.html'],
+  content: ['./homepage.html','./material.html','./center,html','./schedules.html'],
   theme: {
     extend: {},
   },
